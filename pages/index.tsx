086004d30f1cbd0cfd47493b92ceee32a69fb1aa
Kimbo0o <kimkoch1000@gmail.com>
2022-02-22 19:45:16 +0100

    Bugfix: crossOrigin property

--- pages/index.tsx
+++ pages/index.tsx
@@ -54,7 +54,7 @@ const Home: NextPage = () => {
           rel="stylesheet"
           href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
           integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
-          crossorigin=""
+          crossOrigin=""
         />
       </Head>
       <header className={styles.header}>
