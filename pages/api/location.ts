f74811bb1ddae4658e942a96872cdfc885729899
Kimbo0o <kimkoch1000@gmail.com>
2022-02-27 11:23:52 +0100

    Added IPv6 to test

--- pages/api/location.ts
+++ pages/api/location.ts
@@ -30,13 +30,11 @@ export default async function handler(
       };
       res.status(200).json({ data, success: true });
     } else {
-      res
-        .status(422)
-        .json({
-          message: "Input could not be processed",
-          success: false,
-          target: target,
-        });
+      res.status(422).json({
+        message: "Input could not be processed",
+        success: false,
+        target: target,
+      });
     }
   } else {
     res.status(400).json({ message: "No IP/Target provided", success: false });
@@ -49,6 +47,14 @@ export default async function handler(
  * @returns
  */
 const _isValidIP = (value: string): boolean => {
-  const regEx = new RegExp("^((25[0-5]|(2[0-4]|1d|[1-9]|)d)(.(?!$)|$)){4}$");
-  return regEx.test(value);
+  const regExV4 = new RegExp("^((25[0-5]|(2[0-4]|1d|[1-9]|)d)(.(?!$)|$)){4}$");
+  const isV4 = regExV4.test(value);
+  if (isV4) {
+    return true;
+  } else {
+    const regEXV6 = new RegExp(
+      "(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]).){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]).){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))"
+    );
+    return regEXV6.test(value);
+  }
 };
