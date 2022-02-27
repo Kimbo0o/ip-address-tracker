739d12450bfd04f9b73238e2b98ebe80e232ce36
Kimbo0o <kimkoch1000@gmail.com>
2022-02-27 20:35:54 +0100

    Updated Map styles + Icon

--- components/Map/Map.tsx
+++ components/Map/Map.tsx
@@ -1,20 +1,22 @@
 import styles from "./Map.module.scss";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, TileLayer } from "react-leaflet";
 import { LatLngExpression } from "leaflet";
+import L from "leaflet";
 
 const Map: React.FC<{ lat: number; lng: number }> = (props) => {
   const position: LatLngExpression = [props.lat, props.lng];
+  const customIcon = new L.Icon({
+    iconUrl: "/icon-location.svg",
+    iconRetinaUrl: "/icon-location.svg",
+    iconSize: new L.Point(50, 60),
+  });
   return (
     <MapContainer className={styles.leaflet} center={position} zoom={13}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={position}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      <Marker icon={customIcon} position={position}></Marker>
     </MapContainer>
   );
 };
